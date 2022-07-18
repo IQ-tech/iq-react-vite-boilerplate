5f272c21e8bf4eb72ed639cac52eaa778f3edb3b
Ruan Berté <berte.ruan@gmail.com>
2022-07-18 18:15:13 -0300

    chore: install and configure jest and react-testing-library

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  roots: ['<rootDir>/src/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main.tsx',
+  ],
+  coverageDirectory: '<rootDir>/coverage/',
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '(.*).d.ts$'],
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'jest-transform-stub',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+  },
+}
+
+export default config
