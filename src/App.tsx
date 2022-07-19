11dc256d08a21d2580f8a41a21afe84f00ba8119
Ruan Berté <berte.ruan@gmail.com>
2022-07-19 15:19:48 -0300

    feat: install and configure vite pwa

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/images/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Jest</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['images/*'],
+      manifest: {
+        'name': 'IQ React + Vite Boilerlate',
+        'short_name': 'React + Vite Boilerlate',
+        'icons': [
+          {
+            'src': '/images/android-chrome-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png',
+            'purpose': 'any maskable',
+          },
+          {
+            'src': '/images/android-chrome-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png',
+          },
+        ],
+        'background_color': '#242424',
+        'description':
+          'Boilerlate utilizando Vite, React, Typescript, Jest e Styled Components!',
+        'display': 'fullscreen',
+        'start_url': '/',
+        'theme_color': '#242424',
+      },
+    }),
+  ],
 })
